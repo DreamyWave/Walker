16ecc230d1e3d8642f646de9ef1d551489c5370a
Tiphe0821 <namucho24@gmail.com>
2026-05-11 18:50:57 +0900

    [feat] 플레이어 달리기 추가
    플레이어 체력 기반 달리기 기능 추가
    
    Player.cs (변수 추가)
    - sprintInput
    - isSprint
    - lastSprintTime
    - staminaRecoverCooldown
    - maxStamina
    - stamina
    
    --> 위 변수들과 OnSprint 를 통해 플레이어 달리기, 스테미나 회복, 스테미나 소모 구현
    
    - 위와 같이 처리한 이유는 기존 체력 자체를 달리기에 사용했을 경우 플레이어가 달리다가 탈진으로 죽게 되기 떄문에 피크에서처럼 스테미나의 최대치를 현재 체력으로 설정하되 스테미나는 다 닳더라도 플레이어가 죽지 않도록 스테미나를 별도로 추가했습니다
    
    JHC -> Temp_Prefabs
    - Ghost 프리팹 추가 (몬스터 테스트용)
    
    MonsterChase.cs (코드 수정)
    - 기존 코드 Attack()을 퍼블릭으로 바꿈
    - [fix] 다시 프라이빗으로 되돌림
    
    [etc]
    - 기존 다른 사람 작업 코드 변경사항 없음
    - 위 작업내용 전부 JHC 워크스페이스에서 작업되었습니다

--- Walker/Assets/_WorkSpace/JHC/Scripts/Player.cs
+++ Walker/Assets/_WorkSpace/JHC/Scripts/Player.cs
@@ -6,9 +6,14 @@ public class Player : MonoBehaviour
     [Header("플레이어 움직임 설정")]
     private Vector2 moveInput = Vector2.zero;
     public float moveSpeed = 6.0f;
+    public float sprintSpeed = 8.0f;
     public float jumpPower = 5.0f;
     private bool isJump = false;
     private Rigidbody rb;
+    public float sprintInput = 0;
+    private bool isSprint = false;
+    private float lastSprintTime = 0;
+    private float staminaRecoverCooldown = 3.0f;
 
     [Header("마우스 설정")]
     private Vector2 cameraMoveInput;
@@ -17,15 +22,15 @@ public class Player : MonoBehaviour
     public GameObject playerCameraX;
     public GameObject playerCameraY;
 
-    //[Header("플레이어 체력 설정")]
-    //public int maxHp = 100;
-    //private int currentHp;
-
-    [Header("몬스터 데미지")]
-    public int monsterDmg = 10;
-
     [Header("플레이어 스테이터스")] 
     private PlayerStatus playerStatus;
+
+    // 플레이어 스테미나 (체력과는 별개)
+
+    public int maxStamina;
+    public float stamina;
+    // 체력과는 별개로 만들어둡니다 (필요)
+    // 기존 플레이어의 경우 체력 = 스테미나라는 구조라고는 하지만 스테미나가 다 닿을때까지 달렸을 때 플레이어가 죽진 않습니다
 
 
     private void Awake()
@@ -42,6 +47,9 @@ public class Player : MonoBehaviour
     {
         transform.rotation = Quaternion.identity;       // 시작할 때 항상 정면을 보도록 초기화
         isJump = false;
+
+        maxStamina = playerStatus.maxHp;
+        stamina = maxStamina;
     }
 
     private void OnMove(InputValue value)
@@ -64,45 +72,67 @@ public class Player : MonoBehaviour
         }
     }
 
-    // 배고픔 데미지
-    void HungerDamage()
+    private void OnSprint(InputValue value)
     {
-        playerStatus.TakeDamage(playerStatus.hungerDamage);
+        sprintInput = value.Get<float>();
     }
 
-    void MonsterDamage()
-    {
-        playerStatus.TakeDamage(monsterDmg);
-    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Floor")
         {
             isJump = false;
         }
-
-        if(collision.gameObject.CompareTag("Monster"))
-        {
-            
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(moveInput.x * moveSpeed * Time.deltaTime, 0, moveInput.y * moveSpeed * Time.deltaTime);
+        if (sprintInput > 0)
+        {
+            if (stamina > 0)
+            {
+            isSprint = true;
+            lastSprintTime = Time.time;
+            }
+            else
+            {
+                isSprint = false;
+            }
+        }
+        else
+        {
+            isSprint = false;
+            if (Time.time > staminaRecoverCooldown + lastSprintTime)
+            {
+                stamina += (Time.deltaTime * 4);
+                if(stamina > maxStamina)
+                {
+                    stamina = maxStamina;
+                }    
+            }
+        }
+
+        if (isSprint)
+        {
+            transform.Translate(moveInput.x * sprintSpeed * Time.deltaTime, 0, moveInput.y * sprintSpeed * Time.deltaTime);
+            stamina -= (Time.deltaTime * 16);
+        }
+        else
+            transform.Translate(moveInput.x * moveSpeed * Time.deltaTime, 0, moveInput.y * moveSpeed * Time.deltaTime);
 
         playerCameraX.transform.localRotation = Quaternion.AngleAxis(cameraMoveInput.x, Vector3.up);
         playerCameraY.transform.localRotation = Quaternion.AngleAxis(cameraMoveInput.y, Vector3.left);
 
+        if(maxStamina > playerStatus.GetCurrentHP())
+        {
+            maxStamina = playerStatus.GetCurrentHP();
+            if( maxStamina < stamina)
+            {
+                stamina = maxStamina;
+            }
+        }
 
-        // rb.AddRelativeForce(moveInput.x * moveSpeed * Time.deltaTime, 0, moveInput.y * moveSpeed * Time.deltaTime);
-        // 이동 후 바로 멈추지 않고 미끄러짐
-
-        //if(currentHp <= 0)
-        //{
-        //    currentHp = 0;
-        //    GameOver();
-        //}
+        //Debug.Log("최대 스테미나 : " + maxStamina + " // 현재 스테미나 : " + stamina);
     }
 }
